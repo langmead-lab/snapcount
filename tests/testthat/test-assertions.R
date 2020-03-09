e83566c34e028512d61893d02a8bac543d3e2bd6
Rone Charles <rone_charles@fastmail.com>
2020-03-08 22:05:30 -0400

    Add additonal assertion tests

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -1,5 +1,16 @@
 context("Test assertions")
 
+test_that("Empty compilation", {
+    sb <- SnaptronQueryBuilder$new()
+    expect_error(sb$query_jx(), "Please set a compilation")
+})
+
+test_that("Empty region", {
+    sb <- SnaptronQueryBuilder$new()
+    sb$compilation("gtex")
+    expect_error(sb$query_jx(), "Please specify query regions")
+})
+
 test_that("Invalid chromosome range", {
     expect_error(query_jx(compilation = "tcga", regions = "ch0:100-200"))
     expect_error(query_jx(compilation = "tcga", regions = "chr23:100-200"))
@@ -10,16 +21,14 @@ test_that("Invalid sample filters", {
     sb <- SnaptronQueryBuilder$new()
     sb$compilation("gtex")
     sb$regions("CD99")
-    sb$sample_filters(SNTS == "Brain")
+    sb$column_filters(SNTS == "Brain")
     expect_error(query_gene(sb),
                  "(is not a valid sample filter)|(Invalid sample filter)")
 })
 
 test_that("Invalid compilation", {
     sb <- SnaptronQueryBuilder$new()
-    sb$compilation("abcde")
-    sb$regions("CD99")
-    expect_error(query_gene(sb), "Invalid compilation")
+    expect_error(sb$compilation("abcde"), "abcde: is not a valid compilation")
 })
 
 test_that("Invalid coordinate modifier", {
@@ -34,8 +43,6 @@ test_that("Invalid SIDs", {
     sb <- SnaptronQueryBuilder$new()
     sb$compilation("gtex")
     sb$regions("CD99")
-    sb$sids(c("1", "2"))
-    expect_error(query_exon(sb), "sids should be whole numbers")
-    sb$sids(c(1.2, 3.4))
-    expect_error(query_exon(sb), "sids should be whole numbers")
+    expect_error(sb$sids(c("1", "2")), "sids should be whole numbers")
+    expect_error(sb$sids(c(1.2, 3.4)), "sids should be whole numbers")
 })
